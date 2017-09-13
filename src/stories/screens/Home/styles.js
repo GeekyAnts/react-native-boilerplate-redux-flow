530fed6e453014ee3772395b578436a80ffab1e9
Shivraj <shivraj@geekyants.com>
2017-09-13 19:59:47 +0530

    initial setup

---
+++ src/stories/screens/Home/styles.js
@@ -0,0 +1,17 @@
+export default {
+	container: {
+		backgroundColor: "#FBFAFA",
+	},
+	row: {
+		flex: 1,
+		alignItems: "center",
+	},
+	text: {
+		fontSize: 20,
+		marginBottom: 15,
+		alignItems: "center",
+	},
+	mt: {
+		marginTop: 18,
+	},
+};
