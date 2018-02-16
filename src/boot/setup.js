639bc54caae624c8c36ab000b012e598b575bb04
akhil-geekyants <akhil@geekyants.com>
2018-02-16 15:37:33 +0530

    Added files after eject

--- src/boot/setup.js
+++ src/boot/setup.js
@@ -1,4 +1,3 @@
-import * as Expo from "expo";
 import * as React from "react";
 import { StyleProvider } from "native-base";
 import { Provider } from "react-redux";
@@ -22,23 +21,8 @@ export default class Setup extends React.Component<Props, State> {
 			isReady: false,
 		};
 	}
-	componentWillMount() {
-		this.loadFonts();
-	}
-	async loadFonts() {
-		await Expo.Font.loadAsync({
-			Roboto: require("native-base/Fonts/Roboto.ttf"),
-			Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
-			Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
-		});
-
-		this.setState({ isReady: true });
-	}
 
 	render() {
-		if (!this.state.isReady || this.state.isLoading) {
-			return <Expo.AppLoading />;
-		}
 		return (
 			<StyleProvider style={getTheme(variables)}>
 				<Provider store={this.state.store}>
